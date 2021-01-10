e0a1046bdcf3c73c2926f3aefce8ac6bc5f2273f
jesspsilva <jps936@hotmail.com>
2021-01-10 18:33:27 +0000

    Add high order component - Aux - to wrap the layout component

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import Aux from '../../hoc/Auxiliary';
 
 const layout = (props) => (
-    <div>
-        Toolbar, SideDrawer, Backdop
-    </div>
-    <main>
-        {props.children}
-    </main>
+    <Aux>
+        <div>
+            Toolbar, SideDrawer, Backdop
+        </div>
+        <main>
+            {props.children}
+        </main>
+    </Aux>
 );
