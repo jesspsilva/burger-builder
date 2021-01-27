4f199a3e6ef61a3a64ce0954bd478253827375db
jesspsilva <jps936@hotmail.com>
2021-01-27 20:37:51 +0000

    Add route to checkout page

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -107,29 +107,31 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-        this.setState({loading: true});
 
-        const order = {
-            ingredients: this.state.ingredients,
-            price: this.state.totalPrice,
-            customer: {
-                name: 'Jessica',
-                address:  {
-                    street: 'Rua',
-                    zipCode: '4562',
-                    country: 'Portugal'
-                },
-                email: 'teste@teste.com',
-                deliveryMethod: 'fast'
-            }
-        }
-        axios.post('/orders.json', order)
-        .then(response => {
-            this.setState({loading: false, purchasing: false});
-        })
-        .catch(err => {
-            this.setState({loading: false, purchasing: false});
-        })
+        // this.setState({loading: true});
+
+        // const order = {
+        //     ingredients: this.state.ingredients,
+        //     price: this.state.totalPrice,
+        //     customer: {
+        //         name: 'Jessica',
+        //         address:  {
+        //             street: 'Rua',
+        //             zipCode: '4562',
+        //             country: 'Portugal'
+        //         },
+        //         email: 'teste@teste.com',
+        //         deliveryMethod: 'fast'
+        //     }
+        // }
+        // axios.post('/orders.json', order)
+        // .then(response => {
+        //     this.setState({loading: false, purchasing: false});
+        // })
+        // .catch(err => {
+        //     this.setState({loading: false, purchasing: false});
+        // })
+        this.props.history.push('/checkout');
     }
 
     render(){
