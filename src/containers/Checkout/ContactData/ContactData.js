b51ddaded66de7f873f2a9ddce3c338ac6cc181c
jesspsilva <jps936@hotmail.com>
2021-01-28 19:40:28 +0000

    Create input component for contact data form

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Button from '../../../components/UI/Button/Button';
 import Spinner from '../../../components/UI/Spinner/Spinner';
+import Input from '../../../components/UI/Input/Input';
 import classes from './ContactData.css';
 import axios from '../../../axios-orders';
 
@@ -8,11 +9,57 @@ import axios from '../../../axios-orders';
 class ContactData extends Component {
 
     state = {
-        name: '',
-        email: '',
-        address: {
-            street: '',
-            postalCode: '',
+        orderForm: {
+            name: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Name'
+                },
+                value: ''
+            },
+            street: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Street'
+                },
+                value: ''
+            },
+            zipCode: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Zip-code'
+                },
+                value: ''
+            },
+            country:{
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Country'
+                },
+                value: ''
+            },
+            email: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'email',
+                    placeholder: 'Email'
+                },
+                value: ''
+            },
+            deliveryMethod: {
+                elementType: 'select',
+                elementConfig: {
+                    options: [
+                        {value: 'fastest', displayValue: 'Fastest'},
+                        {value: 'cheapest', displayValue: 'Cheapest'}
+                    ]
+                },
+                value: ''
+            }
         },
         loading: false
     }
@@ -24,16 +71,6 @@ class ContactData extends Component {
         const order = {
             ingredients: this.props.ingredients,
             price: this.props.price,
-            customer: {
-                name: 'Jessica',
-                address:  {
-                    street: 'Rua',
-                    zipCode: '4562',
-                    country: 'Portugal'
-                },
-                email: 'teste@teste.com',
-                deliveryMethod: 'fast'
-            }
         }
         axios.post('/orders.json', order)
         .then(response => {
@@ -46,12 +83,26 @@ class ContactData extends Component {
     }
 
     render(){
+
+        const formElements = [];
+        for (let key in this.state.orderForm){
+            formElements.push({
+                id: key,
+                config: this.state.orderForm[key]
+            });
+        }
+
+        console.log(formElements);
+
         let form = (
             <form>
-                <input type="text" name="name" placeholder="Your Name"/>
-                <input type="email" name="email" placeholder="Your Email"/>
-                <input type="text" name="street" placeholder="Street"/>
-                <input type="text" name="postal" placeholder="Postal code"/>
+                {formElements.map(formEl => (
+                    <Input 
+                    key={formEl.id}
+                    elementType={formEl.config.elementType} 
+                    elementConfig={formEl.config.elementConfig} 
+                    value={formEl.config.value}/>
+                ))}
                 <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
             </form>
         );
