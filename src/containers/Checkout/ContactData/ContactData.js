ba2e662b770109f3e9147ef185f901a5ab10af8b
jesspsilva <jps936@hotmail.com>
2021-01-31 16:42:08 +0000

    Add more validation to contact form

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -47,7 +47,8 @@ class ContactData extends Component {
                 value: '',
                 validation: {
                     required: true,
-                    minLength: 6,
+                    minLength: 4,
+                    isNumeric: true
                 },
                 valid: false,
                 touched: false
@@ -73,7 +74,8 @@ class ContactData extends Component {
                 },
                 value: '',
                 validation: {
-                    required: true
+                    required: true,
+                    isEmail: true
                 },
                 valid: false,
                 touched: false
@@ -126,6 +128,14 @@ class ContactData extends Component {
         }
         if (rules.minLength) {
             isValid = value.length >= rules.minLength && isValid;
+        }
+        if (rules.isEmail) {
+            const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+            isValid = pattern.test(value) && isValid
+        }
+        if (rules.isNumeric) {
+            const pattern = /^\d+$/;
+            isValid = pattern.test(value) && isValid
         }
         
         return isValid;
