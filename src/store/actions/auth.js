2d447f41600f2146f197a5aa0d96b3f5d3048ad7
jesspsilva <jps936@hotmail.com>
2021-02-02 21:50:24 +0000

    Get and implement token from firebase

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import * as actionTypes from './actionTypes';
 
 export const authStart = () => {
@@ -23,5 +24,18 @@ export const authFail = (error) => {
 export const auth = (email, password) => {
     return dispatch => {
         dispatch(authStart());
+        const authData = {
+            email: email,
+            password: password,
+            returnSecureToken: true
+        }
+        axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyD2nDrBcLf46vVLxBUetGi9U64r4Fx17qA', authData)
+        .then(res => {
+            console.log(res);
+            dispatch(authSuccess(res.data));
+        })
+        .catch(err => {
+            dispatch(authFail(err));
+        })
     }
 }
