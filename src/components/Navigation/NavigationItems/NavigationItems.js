5c8a7e45cdb62b6946b7fed3df26181948dacac3
jesspsilva <jps936@hotmail.com>
2021-02-03 18:44:03 +0000

    Redirect logged users

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -6,7 +6,7 @@ const navigationItems = (props) => (
     console.log(props),
     <ul className={classes.NavigationItems}>
         <NavigationItem link="/" exact>Burger Builder</NavigationItem>
-        <NavigationItem link="/orders">My Orders</NavigationItem>
+        {props.isAuthenticated ? <NavigationItem link="/orders">My Orders</NavigationItem> : null}
         {!props.isAuthenticated ? 
         <NavigationItem link="/login">Login</NavigationItem> : 
         <NavigationItem link="/logout">Logout</NavigationItem>}
